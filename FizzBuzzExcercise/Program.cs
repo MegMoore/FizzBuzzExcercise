70dbc27ad2c994a8660f5c276095c54e1e85be67
Megan Moore <meganmoore.dev@gmail.com>
2023-08-18 11:31:17 -0400

    Add project files.

---
+++ FizzBuzzExcercise/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+for (var idx = 1; idx <= 30; idx++)
+{
+    if (idx % 3 == 0 && idx % 5 == 0)
+    {
+        Console.WriteLine($"FizzBuzz");
+        continue;
+    }
+    if (idx % 3 == 0)
+    {
+        Console.WriteLine($"Fizz");
+        continue;
+    }
+
+    if (idx % 5 == 0)
+    {
+        Console.WriteLine($"Buzz");
+        continue;
+    }
+    Console.WriteLine($"{idx} ");
+
+}
+Console.WriteLine("\nDone...");
